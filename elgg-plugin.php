a6aa9a791b290c4d319a55733eea19da3f4ddc5e
Ismayil Khayredinov <ismayil.khayredinov@gmail.com>
2026-05-09 14:56:07 +0200

    migrate(7.x): update plugin for Elgg 7.x compatibility
    - Rule 000: bump elgg/elgg to ~7.0.0, php to >=8.3, add ext-intl; update version to 7.0.0; update Dockerfile to php:8.3-apache, elgg-composer.json to ^7.0
    - Rule 009: remove elgg-button-action from menu item link class

--- elgg-plugin.php
+++ elgg-plugin.php
@@ -4,7 +4,7 @@ return [
 	'bootstrap' => \hypeJunction\Discussions\Bootstrap::class,
 
 	'plugin' => [
-		'version' => '6.0.0',
+		'version' => '7.0.0',
 		'dependencies' => [
 			'discussions' => [
 				'position' => 'after',

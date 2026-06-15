cf1ca66b06e82de68cc0df522e70b0c7813b84a2
Ismayil Khayredinov <ismayil.khayredinov@gmail.com>
2026-06-15 18:02:03 +0200

    fix(4.x): pass collection display name as page title in discussions/post
    $title was never assigned -> elgg_view_page(null) TypeError (500) on Elgg 7
    strict string signature. Use $collection->getDisplayName() (already in scope,
    passed to the layout). Same null-title class as the hypelists collection resources.

--- views/default/resources/discussions/post.php
+++ views/default/resources/discussions/post.php
@@ -54,6 +54,6 @@ $layout = elgg_view_layout('default', [
 	'target' => $entity,
 ]);
 
-echo elgg_view_page($title, $layout, 'default', [
+echo elgg_view_page($collection->getDisplayName(), $layout, 'default', [
 	'collection' => $collection,
 ]);
